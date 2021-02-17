a6dfee5ddf1c519cdedede7ba3fc3f59179875e3
EDToaster <howard3061@gmail.com>
2021-02-16 16:42:26 -0800

    fix text colors

--- loader.lua
+++ loader.lua
@@ -97,10 +97,12 @@ local function state_balance()
     local y = 2
     for i, type in ipairs(point_types) do
         mon.setCursorPos(1, y)
+        mon.setTextColor(colors.lightBlue)
         mon.write(type..": "..d.read(type))
         y = y + 1
     end
 
+    mon.setTextColor(colors.lightBlue)
     mon.setCursorPos(1, y)
     mon.write("Please withdraw your card")
     os.pullEvent("disk_eject")
